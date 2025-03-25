d7db3728925fb53c0eae0b03ed32b0dd3a87d13f
Mauro Diaz <127547790+maurodiaz7@users.noreply.github.com>
2025-03-25 15:08:38 -0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,21 @@
 import { base } from '$app/paths';  
 alert(`Base path: ${base}`);
 let pages = [
-  { url: "/{{base}}", title: "Home" },
-  { url: "{{base}}/projects", title: "Projects" },
-  { url: "{{base}}/contact", title: "Contact" },
-  { url: "{{base}}/resume", title: "Resume" },
+  { url: "/", title: "Home" },
+  { url: "/projects", title: "Projects" },
+  { url: "/contact", title: "Contact" },
+  { url: "/resume", title: "Resume" },
   { url: "https://github.com/Mike-Kowalski", title: "Github" }
 ];
 </script>
 <nav>
   {#each pages as p}
-  <a href="{p.url}">{p.title}</a>
+  <a href={p.url.startsWith("http") ? p.url : ${base}${p.url}}
+    class:current={$page.route.id === p.url}
+    target={p.url.startsWith("http") ? "_blank" : undefined}
+  >
+    {p.title}
+  </a>
   {/each}
 </nav>
 <slot />
