4210aedff0fb79d07e813df86d8d3a1fda67f9c7
Mauro Diaz <127547790+maurodiaz7@users.noreply.github.com>
2025-03-25 13:56:52 -0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@ let pages = [
   { url: "./projects", title: "Projects" },
   { url: "./contact", title: "Contact" },
   { url: "./resume", title: "Resume" },
-  { url: "https://github.com/Mike-Kowalski",", title: "Github" }
+  { url: "https://github.com/Mike-Kowalski", title: "Github" }
 ];
 </script>
 <nav>
