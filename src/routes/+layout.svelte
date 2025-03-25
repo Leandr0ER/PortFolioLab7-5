0d95605f95e70ccd2751dca7d3486108272d5597
Mauro Diaz <127547790+maurodiaz7@users.noreply.github.com>
2025-03-25 13:54:57 -0300

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,15 @@
-HOLA <slot />
+<script>
+let pages = [
+  { url: "./", title: "Home" },
+  { url: "./projects", title: "Projects" },
+  { url: "./contact", title: "Contact" },
+  { url: "./resume", title: "Resume" },
+  { url: "https://github.com/Mike-Kowalski",", title: "Github" }
+];
+</script>
+<nav>
+  {#each pages as p}
+  <a href="{p.url}">{p.title}</a>
+  {/each}
+</nav>
+<slot />
