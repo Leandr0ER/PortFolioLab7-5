77c74f41ee0005f275b1976cadc149c5c5831041
Leighton Estrada <lestradar@uni.edu.pe>
2025-04-25 00:10:27 -0300

    Lab 7.5 upd07 update lab7

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -66,7 +66,7 @@ async function dotInteraction (index, evt) {
 }
 
 onMount(async () => {
-	data = await d3.csv("/loc.csv", row => ({
+	data = await d3.csv("./loc.csv", row => ({
     	...row,
     	line: Number(row.line), // or just +row.line
     	depth: Number(row.depth),
