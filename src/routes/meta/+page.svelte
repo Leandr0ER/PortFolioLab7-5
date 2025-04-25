3305bb4639ed0e8a5b255660a3b6351c5d2155ab
Leighton Estrada <lestradar@uni.edu.pe>
2025-04-25 02:36:44 -0300

    Lab 7.5 upd11 update Circle Stable

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -1,159 +1,164 @@
 <script>
-import * as d3 from "d3";
-import { onMount } from "svelte";
+    import * as d3 from "d3";
+    import { onMount } from "svelte";
 
-import {
-    computePosition,
-    autoPlacement,
-    offset,
-} from '@floating-ui/dom';
+    import {
+        computePosition,
+        autoPlacement,
+        offset,
+    } from '@floating-ui/dom';
 
-import Bar from '$lib/Bar.svelte';
+    import Bar from '$lib/Bar.svelte';
 
-let data = [];
-let commits = [];
-let width = 1000, height = 600;
-let margin = {top: 10, right: 10, bottom: 30, left: 20};
-let xAxis, yAxis;
-let yAxisGridlines;
-let hoveredIndex = -1;
-let cursor = {x: 0, y: 0};
+    let data = [];
+    let commits = [];
+    let width = 1000, height = 600;
+    let margin = {top: 10, right: 10, bottom: 30, left: 20};
+    let xAxis, yAxis;
+    let yAxisGridlines;
+    let hoveredIndex = -1;
+    let cursor = {x: 0, y: 0};
 
-$: hoveredCommit = commits[hoveredIndex] ?? hoveredCommit ?? {};
+    let commitProgress = 100;
+    // MOVER ESTA DEFINICIÓN PARA QUE NO DEPENDA DE minDate/maxDate INICIALMENTE
+    $: timeScale = d3.scaleTime().range([0, 100]);
+    $: commitMaxTime = timeScale.invert(commitProgress);
 
-let usableArea = {
-    top: margin.top,
-    right: width - margin.right,
-    bottom: height - margin.bottom,
-    left: margin.left
-};
-usableArea.width = usableArea.right - usableArea.left;
-usableArea.height = usableArea.bottom - usableArea.top;
+    $: filteredCommits = commits.filter(commit => commit.datetime <= commitMaxTime);
+    $: filteredLines = data.filter(line => new Date(line.datetime) <= commitMaxTime);
 
-let commitTooltip;
+    // CALCULAR minDate y maxDate DESPUÉS DE FILTRAR LOS COMMITS
+    $: minDate = filteredCommits.length > 0 ? d3.min(filteredCommits.map(d => d.date)) : new Date();
+    $: maxDate = filteredCommits.length > 0 ? d3.max(filteredCommits.map(d => d.date)) : new Date();
+    $: maxDatePlusOne = new Date(maxDate);
+    $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
 
-let tooltipPosition = {x: 0, y: 0};
-
-let clickedCommits = [];
-
-let commitProgress = 100; 
-
-async function dotInteraction (index, evt) {
-    let hoveredDot = evt.target;
-    if (evt.type === "mouseenter") {
-        hoveredIndex = index;
-        cursor = {x: evt.x, y: evt.y};
-        tooltipPosition = await computePosition(hoveredDot, commitTooltip, {
-            strategy: "fixed", // because we use position: fixed
-            middleware: [
-                offset(5), // spacing from tooltip to dot
-                autoPlacement() // see https://floating-ui.com/docs/autoplacement
-            ],
-        });        }
-    else if (evt.type === "mouseleave") {
-        hoveredIndex = -1
+    // ACTUALIZAR EL DOMINIO DE timeScale CUANDO minDate o maxDate CAMBIEN
+    $: if (minDate && maxDate) {
+        timeScale.domain([minDate, maxDate]);
     }
-	else if (evt.type === "click") {
-    let commit = commits[index]
-    if (!clickedCommits.includes(commit)) {
-        // Add the commit to the clickedCommits array
-        clickedCommits = [...clickedCommits, commit];
+
+    $: xScale = d3.scaleTime()
+        .domain([minDate, maxDatePlusOne])
+        .range([usableArea.left, usableArea.right])
+        .nice();
+
+    $: yScale = d3.scaleLinear()
+        .domain([24, 0])
+        .range([usableArea.bottom, usableArea.top]);
+
+    $: hoveredCommit = filteredCommits[hoveredIndex] ?? hoveredCommit ?? {};
+
+    let usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left
+    };
+    usableArea.width = usableArea.right - usableArea.left;
+    usableArea.height = usableArea.bottom - usableArea.top;
+
+    let commitTooltip;
+
+    let tooltipPosition = {x: 0, y: 0};
+
+    let clickedCommits = [];
+
+    async function dotInteraction (index, evt) {
+        let hoveredDot = evt.target;
+        const commit = filteredCommits[index];
+        if (evt.type === "mouseenter") {
+            hoveredIndex = index;
+            cursor = {x: evt.x, y: evt.y};
+            tooltipPosition = await computePosition(hoveredDot, commitTooltip, {
+                strategy: "fixed",
+                middleware: [
+                    offset(5),
+                    autoPlacement()
+                ],
+            });
+        }
+        else if (evt.type === "mouseleave") {
+            hoveredIndex = -1
+        }
+        else if (evt.type === "click") {
+        if (!clickedCommits.includes(commit)) {
+            clickedCommits = [...clickedCommits, commit];
+        }
+        else {
+                clickedCommits = clickedCommits.filter(c => c !== commit);
+        }
     }
-    else {
-            // Remove the commit from the array
-            clickedCommits = clickedCommits.filter(c => c !== commit);
+
     }
-}
 
-}
+    onMount(async () => {
+        data = await d3.csv("./loc.csv", row => ({
+            ...row,
+            line: Number(row.line),
+            depth: Number(row.depth),
+            length: Number(row.length),
+            date: new Date(row.date + "T00:00" + row.timezone),
+            datetime: new Date(row.datetime)
+        }));
 
-onMount(async () => {
-	data = await d3.csv("./loc.csv", row => ({
-    	...row,
-    	line: Number(row.line), // or just +row.line
-    	depth: Number(row.depth),
-    	length: Number(row.length),
-    	date: new Date(row.date + "T00:00" + row.timezone),
-    	datetime: new Date(row.datetime)
-	}));
-	
-	commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
-    	let first = lines[0];
-    	let {author, date, time, timezone, datetime} = first;
-    	let ret = {
-        	id: commit,
-        	url: "https://github.com/Leandr0ER/PortFolioLab7-5/commit" + commit,
-        	author, date, time, timezone, datetime,
-        	hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-        	totalLines: lines.length
-	    };
+        commits = d3.groups(data, d => d.commit).map(([commit, lines]) => {
+            let first = lines[0];
+            let {author, date, time, timezone, datetime} = first;
+            let ret = {
+                id: commit,
+                url: "https://github.com/Leandr0ER/PortFolioLab7-5/commit" + commit,
+                author, date, time, timezone, datetime,
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                totalLines: lines.length
+            };
 
-    	// Like ret.lines = lines
-    	// but non-enumerable so it doesn’t show up in JSON.stringify
-    	Object.defineProperty(ret, "lines", {
-        	value: lines,
-        	configurable: true,
-        	writable: true,
-	        enumerable: false,
-    	});
+            Object.defineProperty(ret, "lines", {
+                value: lines,
+                configurable: true,
+                writable: true,
+                enumerable: false,
+            });
 
-    	return ret;
-	});
-    commits = d3.sort(commits, d => -d.totalLines);
-});
+            return ret;
+        });
+        commits = d3.sort(commits, d => -d.totalLines);
+    });
 
 
 
-$: minDate = d3.min(commits.map(d => d.date));
-$: maxDate = d3.max(commits.map(d => d.date));
-$: maxDatePlusOne = new Date(maxDate);
-$: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+    $: {
+        d3.select(xAxis).call(d3.axisBottom(xScale));
+        d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
+    }
 
-$: xScale = d3.scaleTime()
-              .domain([minDate, maxDatePlusOne])
-              .range([usableArea.left, usableArea.right])
-              .nice();
+    $: {
+        d3.select(yAxisGridlines).call(
+            d3.axisLeft(yScale)
+                .tickFormat("")
+                .tickSize(-usableArea.width)
+        );
+    }
 
-$: yScale = d3.scaleLinear()
-              .domain([24, 0])
-              .range([usableArea.bottom, usableArea.top]);
+    $: rScale = d3.scaleSqrt()
+        .domain(d3.extent(filteredCommits.map(d=>d.totalLines)))
+        .range([2, 30]);
 
+    $: allTypes = Array.from(new Set(filteredLines.map(d => d.type)));
 
-$: {
-    d3.select(xAxis).call(d3.axisBottom(xScale));
-    d3.select(yAxis).call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, "0") + ":00"));
-}
+    $: selectedLines = (clickedCommits.length > 0 ? clickedCommits : filteredCommits).flatMap(d => d.lines);
 
-$: {
-    d3.select(yAxisGridlines).call(
-        d3.axisLeft(yScale)
-          .tickFormat("")
-          .tickSize(-usableArea.width)
+    $: selectedCounts = d3.rollup(
+        selectedLines,
+        v => v.length,
+        d => d.type
     );
-}
 
-$: rScale = d3.scaleSqrt()
-                .domain(d3.extent(commits.map(d=>d.totalLines)))
-                .range([2, 30]);
+    $: languageBreakdown = allTypes.map(type => [type, selectedCounts.get(type) || 0]);
 
-// Step 6.2				
-$: allTypes = Array.from(new Set(data.map(d => d.type)));
-
-$: selectedLines = (clickedCommits.length > 0 ? clickedCommits : commits).flatMap(d => d.lines);
-
-$: selectedCounts = d3.rollup(
-    selectedLines,
-    v => v.length,
-    d => d.type
-);
-
-$: languageBreakdown = allTypes.map(type => [type, selectedCounts.get(type) || 0]);
-
-$: timeScale = d3.scaleTime().domain([minDate,maxDate]).range([0,100]);
-
-$: commitMaxTime = timeScale.invert(commitProgress);
-
-
+    $: totalLOC = filteredLines.length;
+    $: totalFiles = d3.groups(filteredLines, d => d.file).length;
+    $: totalCommits = filteredCommits.length;
 
 </script>
 <svelte:head>
@@ -161,7 +166,7 @@ $: commitMaxTime = timeScale.invert(commitProgress);
 </svelte:head>
 <h1>Meta</h1>
 <p>This page includes stats about the code of this website</p>
-<p>Total lines of code: {data.length}</p>
+<p>Total lines of code: {totalLOC}</p>
 
 <dl class="info tooltip" bind:this={commitTooltip} hidden={hoveredIndex === -1} style="top: {cursor.y}px; left: {cursor.x}px">
     <dt>Commit</dt>
@@ -177,7 +182,6 @@ $: commitMaxTime = timeScale.invert(commitProgress);
     <dd>{ hoveredCommit.time }</dd>
 </dl>
 
-<!-- Lab 7.5 -->
 <div class="slider-container">
     <div class="slider">
         <label for="slider">Show commits until:</label>
@@ -188,25 +192,25 @@ $: commitMaxTime = timeScale.invert(commitProgress);
 
 
 <svg viewBox="0 0 {width} {height}">
-	<g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
-	<g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
-	<g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
+    <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+    <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+    <g class="gridlines" transform="translate({usableArea.left}, 0)" bind:this={yAxisGridlines} />
     <g class="dots">
-		{#each commits as commit, index }
-		<circle
-			class:selected={ clickedCommits.includes(commit) }
-			on:click={ evt => dotInteraction(index, evt) }
-			on:mouseenter={evt => dotInteraction(index, evt)}
-			on:mouseleave={evt => dotInteraction(index, evt)}
-			cx={ xScale(commit.datetime) }
-			cy={ yScale(commit.hourFrac) }
-			r={ rScale(commit.totalLines) }
-			fill="steelblue"
-			fill-opacity="0.5"
-			/>		
-		{/each}
-		
-	</g>			
+        {#each filteredCommits as commit, index (commit.id) }
+        <circle
+            class:selected={ clickedCommits.includes(commit) }
+            on:click={ evt => dotInteraction(index, evt) }
+            on:mouseenter={evt => dotInteraction(index, evt)}
+            on:mouseleave={evt => dotInteraction(index, evt)}
+            cx={ xScale(commit.datetime) }
+            cy={ yScale(commit.hourFrac) }
+            r={ rScale(commit.totalLines) }
+            fill="steelblue"
+            fill-opacity="0.5"
+            style="--r: {rScale(commit.totalLines)};"
+            />
+        {/each}
+    </g>
 </svg>
 <Bar data={languageBreakdown} width={width} />
 
@@ -214,44 +218,44 @@ $: commitMaxTime = timeScale.invert(commitProgress);
     <h2>Summary</h2>
     <dl class="stats">
     <dt>Total <abbr title="Lines of code">LOC</abbr></dt>
-    <dd>{data.length}</dd>
+    <dd>{totalLOC}</dd>
     <dt>Files</dt>
-    <dd>{d3.groups(data, d => d.file).length}</dd>
+    <dd>{totalFiles}</dd>
     <dt>Commits</dt>
-    <dd>{d3.groups(data, d => d.commit).length}</dd>
-    </dl>	
+    <dd>{totalCommits}</dd>
+    </dl>
 
 </section>
 
 <style>
-	dl{
-		display: grid;
-		grid-template-columns: auto;
-	}
-	dt{
-		grid-row: 1;
-		font-family: inherit;
-		font-weight: bold;
-		color: var(--border-gray);
-		text-transform: uppercase;
-	}
-	dd{
-		font-family: inherit;
-		font-weight: bold;
-	}
-	section{
-		border-width:0.15em;
-		border-style:solid;
-		border-color:var(--border-gray);
-		padding-left: 1em;
-		padding-right: 1em;
-	}
-	svg {
+    dl{
+        display: grid;
+        grid-template-columns: auto;
+    }
+    dt{
+        grid-row: 1;
+        font-family: inherit;
+        font-weight: bold;
+        color: var(--border-gray);
+        text-transform: uppercase;
+    }
+    dd{
+        font-family: inherit;
+        font-weight: bold;
+    }
+    section{
+        border-width:0.15em;
+        border-style:solid;
+        border-color:var(--border-gray);
+        padding-left: 1em;
+        padding-right: 1em;
+    }
+    svg {
         overflow: visible;
     }
-	.gridlines {
-    	stroke-opacity: .2;
-	}
+    .gridlines {
+        stroke-opacity: .2;
+    }
 
 .info{
     display: grid;
@@ -263,8 +267,8 @@ $: commitMaxTime = timeScale.invert(commitProgress);
     backdrop-filter: blur(10px);
     padding:10px;
 
-	transition-duration: 500ms;
-    transition-property: opacity, visibility;
+    transition-duration: 200ms; /* MODIFIED HERE */
+    transition-property: opacity, visibility, r; /* MODIFIED HERE */
 
     &[hidden]:not(:hover, :focus-within) {
         opacity: 0;
@@ -291,9 +295,14 @@ $: commitMaxTime = timeScale.invert(commitProgress);
 }
 
 circle {
-    transition: 200ms;
-	transform-origin: center;
-	transform-box: fill-box;
+    transition: all 200ms, /* MODIFIED HERE */
+                r calc(var(--r) * 100ms); /* MODIFIED HERE */
+    transform-origin: center;
+    transform-box: fill-box;
+
+    @starting-style { /* ADD THIS BLOCK */
+        r: 0;
+    }
 
     &:hover {
         transform: scale(1.5);
@@ -306,17 +315,17 @@ circle {
 
 /* Lab 7.5 */
 .slider-container{
-	display:grid;
+    display:grid;
 }
 .slider{
-	display: flex;
+    display: flex;
 }
 #slider{
-	flex:1;
+    flex:1;
 }
 .time-label{
-	font-size: 0.75em;
-	text-align: right;
+    font-size: 0.75em;
+    text-align: right;
 }
 
-</style>	
+</style>
